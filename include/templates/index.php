74d67de1e30a3169ead3fb232c962c83454a353b
Bill Bellon <billb@ssec.wisc.edu>
2018-12-14 10:43:07 -0600

    improved mobile layout and tweaked colors of banner

--- include/templates/index.php
+++ include/templates/index.php
@@ -17,16 +17,16 @@
 <div id="banner">
 	<h1 id="title">PDF</h1>
 	<div id="controls">
-		<span class="control-group">
-			<a href="<?php echo $this->url;?>"><button>Download PDF</button></a>
+		<span class="control-group" id="download-button">
+			<a href="<?php echo $this->url;?>"><button class="btn btn-light">Download PDF</button></a>
 		</span>
 		<span id="zoom-buttons" class="control-group">
-			<button id="zoomout">-</button>
-			<button id="zoomin">+</button>
+			<button class="btn btn-light" id="zoomout">-</button>
+			<button class="btn btn-light" id="zoomin">+</button>
 		</span>
 		<span id="prevnext-buttons" class="control-group">
-			<button id="prev">&laquo; Previous</button>
-			<button id="next">Next &raquo;</button>
+			<button class="btn btn-light" id="prev">&laquo; Previous</button>
+			<button class="btn btn-light" id="next">Next &raquo;</button>
 		</span>
 		<span id="page-num-container" class="control-group">
 			Page: <span id="page_num"></span> / <span id="page_count"></span>
